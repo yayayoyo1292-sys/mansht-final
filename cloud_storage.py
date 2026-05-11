6943a0f2510ead5b65ad09a9a485dea1fe7b9344
yayayoyo1292-sys <yayayoyo1292@gmail.com>
2026-05-11 17:57:06 +0300

    Create cloud_storage.py

---
+++ cloud_storage.py
@@ -0,0 +1,20 @@
+import io
+import os
+from supabase import create_client
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+def upload_image(image, filename):
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    supabase.storage.from_("news-images").upload(
+        path=filename,
+        file=buffer.read(),
+        file_options={"content-type": "image/png"}
+    )
