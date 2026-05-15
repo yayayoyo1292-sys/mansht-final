8d6a9e6098bf8e0a309916103ea6f579c9909c91
yayayoyo1292-sys <yayayoyo1292@gmail.com>
2026-05-15 21:06:59 +0300

    Update db.py

--- DB/db.py
+++ DB/db.py
@@ -2,17 +2,19 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
+from psycopg2 import pool
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+_pool = pool.ThreadedConnectionPool(minconn=2, maxconn=10, dsn=DATABASE_URL)
 def get_conn():
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL is missing in .env")
     return psycopg2.connect(DATABASE_URL)
 
 def db_execute(query, params=None, fetch=False):
-    conn = get_conn()
+    conn = _pool.getconn()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
 
     try:
@@ -32,5 +34,4 @@ def db_execute(query, params=None, fetch=False):
         raise e
 
     finally:
-        cursor.close()
-        conn.close()
+        _pool.putconn(conn)
