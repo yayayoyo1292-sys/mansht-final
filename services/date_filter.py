623c4578e8133ae8f30f57f170dfe641095e1995
yayayoyo1292-sys <yayayoyo1292@gmail.com>
2026-05-16 17:57:54 +0300

    Update date_filter.py

--- services/date_filter.py
+++ services/date_filter.py
@@ -1,11 +1,6 @@
-from config.settings import (
-    FACEBOOK_START_DATE,
-    FACEBOOK_END_DATE
-)
+
+from config.settings import FACEBOOK_START_DATE, FACEBOOK_END_DATE
 
 
-def is_within_range(article_date):
-
-    return (
-        FACEBOOK_START_DATE <= article_date <= FACEBOOK_END_DATE
-    )
+def is_within_range(article_date) -> bool:
+    return FACEBOOK_START_DATE <= article_date <= FACEBOOK_END_DATE
